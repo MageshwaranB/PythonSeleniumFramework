3ef9b77f25bc02f908baa932a2667960d4e9444c
Mageshwaran <mageshbalraj@gmail.com>
2023-11-28 21:53:54 +0530

    fixing the xpath issue

--- pom/pages/page_product.py
+++ pom/pages/page_product.py
@@ -1,6 +1,6 @@
 import pytest
 from selenium.webdriver.common.by import By
-
+import time
 from pom.pages.base_page import BasePage
 
 
@@ -12,14 +12,17 @@ class ProductPage(BasePage):
     all_items = (By.XPATH, "//div[@class='inventory_item']")
     product_text = (By.XPATH, "//div[@class='inventory_item_name']")
     cart_count=(By.XPATH,"//span[contains(@class,'shopping_cart_badge')]")
-
+    cart_button=(By.XPATH,"//button[contains(@class,'btn_inventory')][{i}]")
+    
     def count_no_of_items(self):
         return self.get_elements_count(self.all_items)
 
     def add_all_items_to_basket(self):
-        pytest.set_trace()
-        for i in range(1,self.count_no_of_items()+1):
-            self.click((By.XPATH,"//button[contains(@class,'btn_inventory')]["+str(i)+"]"))
+        for i in range(0,self.count_no_of_items()):
+            count=i+1
+            cart=(By.XPATH,"(//button[contains(@class,'btn_inventory')])["+str(count)+"]")
+            self.click(cart)
+            time.sleep(2)
         return int(self.get_cart_count())
 
     def get_cart_count(self):
